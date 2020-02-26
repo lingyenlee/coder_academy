8dfea77e3be7adf2b2fd6dd412c3012bcf8c4e17
lingyenlee <lingyenlee@gmail.com>
2020-02-26 13:44:08 +1100

    first commit

---
+++ ruby/code_260220.rb
@@ -0,0 +1,21 @@
+name = "G.e.n.n.y"
+
+age = 33
+
+is_registered_voter = true
+
+# puts name.swapcase
+
+# puts name.length
+
+# puts name[1]
+
+name_split = name.split(".")
+puts name_split
+puts name_split.reverse
+
+# hashes
+
+countries = {"usa" => "United States of America", "aus" => "australia"}
+puts countries.class
+puts countries["usa"]
