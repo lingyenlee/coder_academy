282983f7a769d263e2cda0ba6e7fec8ba5f8cc0c
lingyenlee <lingyenlee@gmail.com>
2020-04-23 20:08:54 +1000

    many-to-many

--- rails/07_toy_store/app/models/toy.rb
+++ rails/07_toy_store/app/models/toy.rb
@@ -1,4 +1,7 @@
 class Toy < ApplicationRecord
     belongs_to :user
     belongs_to :manufacturer
+    has_many :toys_categories
+    has_many :categories, through: :toys_categories
+    has_many :images, as: :imageable
 end
--- rails/07_toy_store/db/schema.rb
+++ rails/07_toy_store/db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_061427) do
+ActiveRecord::Schema.define(version: 2020_04_23_100128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "imageable_type"
+    t.bigint "imageable_id"
+    t.string "url"
+    t.date "date_taken"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["imageable_type", "imageable_id"], name: "index_images_on_imageable_type_and_imageable_id"
+  end
 
   create_table "manufacturers", force: :cascade do |t|
     t.string "name"
@@ -34,6 +50,15 @@ ActiveRecord::Schema.define(version: 2020_04_21_061427) do
     t.index ["user_id"], name: "index_toys_on_user_id"
   end
 
+  create_table "toys_categories", force: :cascade do |t|
+    t.bigint "toy_id"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_toys_categories_on_category_id"
+    t.index ["toy_id"], name: "index_toys_categories_on_toy_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password"
@@ -43,4 +68,6 @@ ActiveRecord::Schema.define(version: 2020_04_21_061427) do
 
   add_foreign_key "toys", "manufacturers"
   add_foreign_key "toys", "users"
+  add_foreign_key "toys_categories", "categories"
+  add_foreign_key "toys_categories", "toys"
 end
